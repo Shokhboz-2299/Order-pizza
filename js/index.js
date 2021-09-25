775ef4990d858c08e3c5cff6986dd56fe6f8ffc0
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2021-09-25 14:58:12 +0500

    Add bread select

--- js/index.js
+++ js/index.js
@@ -1,74 +1,33 @@
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// for (var number of numbers) {
-//   console.log(number);
+// htmldan elementlarni chaqirib olish
+
+var elPizzaForm =document.querySelector(".pizza-form");
+var elSelectBread = elPizzaForm.querySelector(".bread-select");
+var elBreadResult = document.querySelector(".breadResult");
+var elInputRadioWrapperResult = document.querySelector(".inputRadioWrapperResult");
+var elInputCheckboxWrapper = elPizzaForm.querySelector(".inputCheckboxWrapper");
+var elInputCheckboxWrapper2 = elPizzaForm.querySelector(".inputCheckboxWrapper2");
+
+var elInputRadioWrapper = elPizzaForm.querySelector(".inputRadioWrapper");
+
+// how to make bread 
+
+// var optionBread =["Yupqa", "Qalin", "Farqiyoq"];
+
+// for (var i = 0; i < optionBread.length; i++) {
+//   var OptionItem = document.createElement('option');
+//   OptionItem.value = optionBread[i];
+//   OptionItem.textContent = optionBread[i];
+//   OptionItem.classList = "text-denger";
+
+//   elBreadResult.append(OptionItem);
 // }
+
+elSelectBread.addEventListener('change', function () {
+    elBreadResult.textContent = this.value;
+  });
+
+
 
 /* ==================== */
 
-// var elsInputCheckbox = document.querySelectorAll('[type="checkbox"]');
-
-// console.log(elsInputCheckbox);
-
-// for (var i = 0; i < 5; i++) {
-//   console.log(numbers[i]);
-// }
-
-// var elFruitsList = document.querySelector(".js-fruits-list");
-var elInputWrapper = document.querySelector(".js-input-wrapper");
-
-var fruits = [
-  "olma",
-  "nok",
-  "anor",
-  "shaftoli",
-  "o'rik",
-  "olcha",
-  "gilos",
-  "tut",
-  "uzum",
-  "banan",
-  "mango",
-  "kivi",
-  "apelsin",
-  "mandarin",
-];
-var checkedFruits = [];
-
-for (var i = 0; i < fruits.length; i++) {
-  var newelFruitsLabel = document.createElement("label");
-  var newElFruitsCheckbox = document.createElement("input");
-  var newElFruitsSpan = document.createElement("span");
-
-  newElFruitsCheckbox.type = "checkbox";
-  newElFruitsCheckbox.id = i;
-  newElFruitsCheckbox.required = true;
-  newElFruitsCheckbox.value = fruits[i];
-
-  newElFruitsSpan.textContent = fruits[i];
-
-  newElFruitsCheckbox.addEventListener("change", function (evt) {
-    evt.preventDefault();
-
-    var curruntFruit = newElFruitsCheckbox.value;
-    console.log(curruntFruit);
-  });
-
-  newelFruitsLabel.appendChild(newElFruitsCheckbox);
-  newelFruitsLabel.appendChild(newElFruitsSpan);
-
-  elInputWrapper.appendChild(newelFruitsLabel);
-}
-
-var checkboxes = document.querySelectorAll('[type="checkbox"]');
-
-// var checkedElements = function () {
-//   if (newElFruitsCheckbox.checked) {
-//     console.log(checkedFruits.push(newElFruitsCheckbox.value));
-//   }
-// };
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// console.log(numbers);
-// var deleted = numbers.splice(3, 0);
-// console.log(numbers);
-// console.log(deleted);
